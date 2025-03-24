f55f4e12466b5109e0b09d29ab236cd3cbb1a0cc
Atul Kamble <atul_kamble@hotmail.com>
2025-03-24 14:46:00 +0530

    Update db-setup.sh

--- deploy/db-setup.sh
+++ deploy/db-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo yum install mariadb105-devel
-sudo systemctl start mariadb
-sudo systemctl enable mariadb
+sudo yum install mariadb-server -y
+sudo systemctl enable --now mariadb
+sudo systemctl restart mariadb
 git clone https://github.com/atulkamble/3-tier-website.git
 mysql -u root < 3-tier-website/database/init.sql
