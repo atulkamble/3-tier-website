309feb3a5646f69f53a185bb72d51d4a4f8f48e5
Atul Kamble <atul_kamble@hotmail.com>
2025-03-24 17:05:22 +0530

    Update backend-setup.sh

--- deploy/backend-setup.sh
+++ deploy/backend-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -fsSL https://rpm.nodesource.com/setup_18.x | sudo bash -
 sudo yum install -y nodejs git
-git clone https://github.com/atulkamble/3-tier-website.git
+git clone https://github.com/atulkamble/aws-3-tier-website.git
 cd 3-tier-website/backend
 npm init -y
 npm install express mysql2 cors
