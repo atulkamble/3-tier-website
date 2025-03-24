c17eb5f76e98b49a771c1c043b453b7c2fbceebd
Atul Kamble <atul_kamble@hotmail.com>
2025-03-24 14:39:32 +0530

    Update frontend-setup.sh

--- deploy/frontend-setup.sh
+++ deploy/frontend-setup.sh
@@ -3,5 +3,5 @@ sudo yum install -y httpd git
 sudo systemctl start httpd
 sudo systemctl enable httpd
 cd /var/www/html
-git clone https://github.com/your-username/3-tier-website.git
+git clone https://github.com/atulkamble/3-tier-website.git
 cp 3-tier-website/frontend/index.html .
