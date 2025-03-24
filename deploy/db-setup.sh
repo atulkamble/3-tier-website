f52fb23f9474b99538ff2c63cfedaa863e0494ad
Atul Kamble <atul_kamble@hotmail.com>
2025-03-24 14:50:09 +0530

    Update db-setup.sh

--- deploy/db-setup.sh
+++ deploy/db-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo yum install mysql-selinux.noarch -y
-sudo yum install mariadb -y
+sudo yum install mariadb105-server.x86_64 -y
 sudo systemctl enable --now mariadb
 sudo systemctl restart mariadb
 git clone https://github.com/atulkamble/3-tier-website.git
